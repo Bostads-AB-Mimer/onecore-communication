b74cbd05431f8488c0bf98f9394242cf061e5bda
William Ryder <williamryderb@gmail.com>
2024-06-12 10:41:00 +0200

    feat: add endpoint for sending parking space offer emails

--- src/services/infobip-service/adapters/infobip-adapter.ts
+++ src/services/infobip-service/adapters/infobip-adapter.ts
@@ -1,6 +1,6 @@
 import { Infobip, AuthType } from '@infobip-api/sdk'
 import config from '../../../common/config'
-import { Email } from 'onecore-types'
+import { Email, ParkingSpaceOfferEmail } from 'onecore-types'
 
 const infobip = new Infobip({
   baseUrl: config.infobip.baseUrl,
@@ -28,3 +28,38 @@ export const sendEmail = async (message: Email) => {
     throw error
   }
 }
+
+export const sendParkingSpaceOffer = async (email: ParkingSpaceOfferEmail) => {
+  console.log('Sending template email', config.infobip.baseUrl)
+  try {
+    const toField = JSON.stringify({
+      to: email.to,
+      placeholders: {
+        'adress': email.adress,
+        'firstName': email.firstName,
+        'availableFrom': email.availableFrom,
+        'deadlineDate': email.deadlineDate,
+        'rent': email.rent,
+        'type': email.type,
+        'parkingSpaceId': email.parkingSpaceId,
+        'objectId': email.objectId,
+      },
+    });
+    const response = await infobip.channels.email.send({
+      from: 'Bostads Mimer AB <noreply@mimer.nu>',
+      to: toField,
+      templateId: email.hasParkingSpace ? 200000000094058 : 200000000092027,
+      subject: email.subject, // Might be overriden by tempalte
+      text: email.text, // Should be overriden by template, but can be used as fallback
+    });
+    if (response.status === 200) {
+      return response.data;
+    } else {  
+      throw new Error(response.body);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
