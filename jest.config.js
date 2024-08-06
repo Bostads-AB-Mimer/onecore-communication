fde071f6830a607001172fe42c9831043bceede7
momentiris <momentiris@gmail.com>
2024-08-06 15:59:52 +0200

    add test env

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/build/'],
   extensionsToTreatAsEsm: ['.d.ts, .ts'],
+  setupFiles: ['<rootDir>/.jest/env.ts'],
 }
