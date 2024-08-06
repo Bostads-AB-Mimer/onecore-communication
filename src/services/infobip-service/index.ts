672896cf482f09205fb8f4c086d8298418c4dc70
Johannes <erikjohanneskarlsson@gmail.com>
2024-08-06 11:17:01 +0200

    feat 1542: edits api responses to generalised structure and adds metadata

--- src/services/infobip-service/index.ts
+++ src/services/infobip-service/index.ts
@@ -1,51 +1,70 @@
 import KoaRouter from '@koa/router'
 import validator from 'validator'
 import { validator as phoneValidator, normalize } from 'telefonnummer'
-import { sendEmail, sendParkingSpaceOffer, sendParkingSpaceAssignedToOther, sendParkingSpaceOfferSms } from './adapters/infobip-adapter'
-import { Email, ParkingSpaceOfferEmail, ParkingSpaceNotificationEmail, ParkingSpaceOfferSms } from 'onecore-types'
+import {
+  sendEmail,
+  sendParkingSpaceOffer,
+  sendParkingSpaceAssignedToOther,
+  sendParkingSpaceOfferSms,
+} from './adapters/infobip-adapter'
+import {
+  Email,
+  ParkingSpaceOfferEmail,
+  ParkingSpaceNotificationEmail,
+  ParkingSpaceOfferSms,
+} from 'onecore-types'
+import { generateRouteMetadata } from 'onecore-utilities'
 import config from '../../common/config'
 
 export const routes = (router: KoaRouter) => {
   router.post('(.*)/sendMessage', async (ctx) => {
+    const metadata = generateRouteMetadata(ctx)
     const message = ctx.request.body
     if (!isMessageEmail(message)) {
-      ctx.throw(400, 'Message is not an email object')
+      ctx.status = 400
+      ctx.body = { reason: 'Message is not an email object', ...metadata }
       return
     }
     try {
       const result = await sendEmail(message)
       ctx.status = 200
-      ctx.body = result.data
+      ctx.body = { content: result.data, ...metadata }
     } catch (error: any) {
       ctx.status = 500
       ctx.body = {
-        message: error.message,
+        error: error.message,
+        ...metadata,
       }
     }
   })
 
   router.post('(.*)/sendParkingSpaceOffer', async (ctx) => {
+    const metadata = generateRouteMetadata(ctx)
     const emailData = ctx.request.body
     if (!isParkingSpaceOfferEmail(emailData)) {
-      ctx.throw(400, 'Message is not an email object')
+      ctx.status = 400
+      ctx.body = { reason: 'Message is not an email object', ...metadata }
       return
     }
     try {
       const result = await sendParkingSpaceOffer(emailData)
       ctx.status = 200
-      ctx.body = result.data
+      ctx.body = { content: result.data, ...metadata }
     } catch (error: any) {
       ctx.status = 500
       ctx.body = {
-        message: error.message,
+        error: error.message,
+        ...metadata,
       }
     }
   })
   router.post('(.*)/sendNotification', async (ctx) => {
-    const { applicants } = ctx.request.body as { applicants: ParkingSpaceNotificationEmail[] }
+    const { applicants } = ctx.request.body as {
+      applicants: ParkingSpaceNotificationEmail[]
+    }
     if (!Array.isArray(applicants)) {
       ctx.throw(400, 'Message is not an email object')
-      return;
+      return
     }
     try {
       const result = await sendParkingSpaceAssignedToOther(applicants)
@@ -66,14 +85,14 @@ export const routes = (router: KoaRouter) => {
         ctx.throw(400, 'Message is not an sms object')
         return
       }
-  
+
       let phoneNumber = sms.phoneNumber
       if (!phoneValidator(phoneNumber)) {
         ctx.throw(400, 'Invalid phone number')
         return
       }
       phoneNumber = '46' + normalize(phoneNumber).slice(1)
-  
+
       const result = await sendParkingSpaceOfferSms({ ...sms, phoneNumber })
       ctx.status = 200
       ctx.body = result
@@ -84,7 +103,6 @@ export const routes = (router: KoaRouter) => {
       }
     }
   })
-
 }
 
 export const isParkingSpaceOfferEmail = (
@@ -128,7 +146,7 @@ export const isValidSms = (sms: any): sms is ParkingSpaceOfferSms => {
     typeof sms === 'object' &&
     sms !== null &&
     typeof sms.phoneNumber === 'string' &&
-    typeof sms.firstName === 'string' && 
+    typeof sms.firstName === 'string' &&
     typeof sms.deadlineDate === 'string'
-  );
+  )
 }
