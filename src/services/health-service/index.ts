9e489ee51e9d7509c8ddb2c3492ff9c9379f1c44
Johanna Grahn <johanna.grahn@iteam.se>
2024-08-08 10:04:19 +0200

    Feat 1444: More Health Routes
    * Time limit between health checks for each dependency
    * Health check for infobip

--- src/services/health-service/index.ts
+++ src/services/health-service/index.ts
@@ -1,18 +1,112 @@
 import KoaRouter from '@koa/router'
 import { SystemHealth, SystemStatus } from 'onecore-types'
+import config from '../../common/config'
+import { healthCheck as infobipHealthCheck } from '../infobip-service/adapters/infobip-adapter'
+
+const healthChecks: Map<string, SystemHealth> = new Map()
+
+const probe = async (
+  systemName: string,
+  minimumMinutesBetweenRequests: number,
+  checkFunction: Function,
+  activeMessage?: string
+): Promise<SystemHealth> => {
+  let currentHealth = healthChecks.get(systemName)
+
+  if (
+    !currentHealth ||
+    Math.floor(
+      (new Date().getTime() - currentHealth.timeStamp.getTime()) / 60000
+    ) >= minimumMinutesBetweenRequests
+  ) {
+    try {
+      const result = await checkFunction()
+      console.log('result', result)
+      if (result) {
+        currentHealth = {
+          status: result.status,
+          name: result.name,
+          subsystems: result.subsystems,
+          timeStamp: new Date(),
+        }
+      } else {
+        currentHealth = {
+          status: 'active',
+          name: systemName,
+          timeStamp: new Date(),
+        }
+        if (activeMessage) currentHealth.statusMessage = activeMessage
+      }
+    } catch (error: any) {
+      if (error instanceof ReferenceError) {
+        currentHealth = {
+          status: 'impaired',
+          statusMessage: error.message || 'Reference error ' + systemName,
+          name: systemName,
+          timeStamp: new Date(),
+        }
+      } else {
+        currentHealth = {
+          status: 'failure',
+          statusMessage: error.message || 'Failed to access ' + systemName,
+          name: systemName,
+          timeStamp: new Date(),
+        }
+      }
+    }
+
+    healthChecks.set(systemName, currentHealth)
+  }
+  return currentHealth
+}
+
+const subsystems = [
+  {
+    probe: async (): Promise<SystemHealth> => {
+      return await probe(
+        config.health.infobip.systemName,
+        config.health.infobip.minimumMinutesBetweenRequests,
+        infobipHealthCheck
+      )
+    },
+  },
+]
 
 export const routes = (router: KoaRouter) => {
-  router.get('(.*)/health', (ctx) => {
-    const currentSystemStatus: SystemStatus = 'active'
-    const currentSystemStatusMessage = ''
-
+  router.get('(.*)/health', async (ctx) => {
     const health: SystemHealth = {
       name: 'communication',
       status: 'active',
+      timeStamp: new Date(),
+      subsystems: [],
     }
 
-    health.status = currentSystemStatus
-    health.statusMessage = currentSystemStatusMessage
+    // Iterate over subsystems
+    for (const subsystem of subsystems) {
+      const subsystemHealth = await subsystem.probe()
+      health.subsystems?.push(subsystemHealth)
+
+      switch (subsystemHealth.status) {
+        case 'failure':
+          health.status = 'failure'
+          health.statusMessage = 'Failure because of failing subsystem'
+          break
+        case 'impaired':
+          if (health.status !== 'failure') {
+            health.status = 'impaired'
+            health.statusMessage = 'Failure because of impaired subsystem'
+          }
+          break
+        case 'unknown':
+          if (health.status !== 'failure' && health.status !== 'impaired') {
+            health.status = 'unknown'
+            health.statusMessage = 'Unknown because subsystem status is unknown'
+          }
+          break
+        default:
+          break
+      }
+    }
 
     ctx.body = health
   })
